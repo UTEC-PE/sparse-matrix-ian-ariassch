ed2f826de1110760c0b58886c3eb3fb663b0d77e
ian-ariassch <38139729+ian-ariassch@users.noreply.github.com>
2018-11-09 16:15:51 -0500

    set y suma corregidos

--- matrix.h
+++ matrix.h
@@ -7,157 +7,154 @@ using namespace std;
 
 template <typename T>
 class Matrix {
-private:
-    Node<T>* hRows;
-    Node<T>* hColumns;
-    int columns;
-    int rows;
+    private:
+        Node<T>* hRows;
+        Node<T>* hColumns;
+        int columns;
+        int rows;
 
-public:
-    Matrix(){};
-    Matrix(int sizeX, int sizeY)
-    {
-
-        hRows = new Node<T>;
-        hColumns = new Node<T>;
-        columns=sizeX;
-        rows=sizeY;
-        hRows->data=0;
-        hRows->x=0;
-        hRows->y=1;
-        hColumns->data=0;
-        hColumns->x=1;
-        hColumns->y=0;
-        auto *tempn = hColumns;
-        auto *tempd = hRows;
-        for(int x=1; x<=sizeX; x++)
-        {
-            auto *temp = new Node<T>;
-            tempn->next=temp;
-            temp->x=x;
-            temp->y=0;
-            temp->data=x;
-            tempn = temp;
-        }
-        for(int y=1; y<=sizeY; y++)
-        {
-            auto *temp = new Node<T>;
-            tempd->down=temp;
-            temp->x=0;
-            temp->y=y;
-            temp->data=y;
-            tempd = temp;
-        }
-
-    }
-
-    void set(int x, int y, T data)
-    {
-
-        if(x==0 or y==0){
-            cout << "IllegalIndexPosition" << endl;
-            throw;
-        }
-        if(x>columns or y>rows)
-        {
-            cout<< "OutOfRange"<<endl;
-            throw;
-        }
-        else
+    public:
+        Matrix(){};
+        Matrix(int sizeX, int sizeY)
         {
 
-            auto *tempX = hColumns;
-            auto *tempY = hRows;
-            auto *nodo = new Node<T>;
-            nodo->x = x;
-            nodo->y = y;
-            nodo->data = data;
-            for(int xi=0; xi<=columns; xi++)
+            hRows = new Node<T>;
+            hColumns = new Node<T>;
+            columns=sizeX;
+            rows=sizeY;
+            hRows->data=0;
+            hRows->x=0;
+            hRows->y=1;
+            hColumns->data=0;
+            hColumns->x=1;
+            hColumns->y=0;
+            auto *tempn = hColumns;
+            auto *tempd = hRows;
+            for(int x=1; x<=sizeX; x++)
             {
-                if(tempX->x==nodo->x)
-                {
-                    if(tempX->down==NULL)
-                    {
-                        tempX->down=nodo;
-                    }
-
-                    if(tempX->down->y>nodo->y)
-                    {
-                        nodo->down=tempX->down;
-                        tempX->down=nodo;
-                    }
-                    else if(tempX->down->y<nodo->y)
-                    {
-                        tempX->down->down=nodo;
-                    }
-
-                    break;
-                }
-                if(tempX->next!=NULL)
-                {
-                    tempX = tempX->next;
-                }
+                auto *temp = new Node<T>;
+                tempn->next=temp;
+                temp->x=x;
+                temp->y=0;
+                temp->data=x;
+                tempn = temp;
             }
-            for(int yi=0; yi<=rows; yi++)
+            for(int y=1; y<=sizeY; y++)
             {
-                if(tempY->y==nodo->y)
-                {
-                    if(tempY->next==NULL)
-                    {
-                        tempY->next=nodo;
-                    }
-
-                    if(tempY->next->x>nodo->x)
-                    {
-                        nodo->next=tempY->next;
-                        tempY->next=nodo;
-                    }
-                    else if(tempY->next->y<nodo->y)
-                    {
-                        tempY->next->next=nodo;
-                    }
-
-                    break;
-                }
-                if(tempY->down!=NULL)
-                {
-                    tempY = tempY->down;
-                }
-            }
-            cout<<"Created node in"<<" "<<nodo->x<<" "<<nodo->y<<" with data: "<<nodo->data<<endl;
-        }
-
-    };
-    T operator()(int x, int y)
-    {
-        if(x == 0 or y == 0)
-        {
-            cout<<"InvalidIndex"<<endl;
-            throw;
-        }
-        else{
-            auto *temp = hColumns;
-
-            while(temp->x != x and temp->next != NULL)
-            {
-                temp=temp->next;
-            }
-            while(temp->y != y and temp->down != NULL)
-            {
-                temp=temp->down;
+                auto *temp = new Node<T>;
+                tempd->down=temp;
+                temp->x=0;
+                temp->y=y;
+                temp->data=y;
+                tempd = temp;
             }
 
-            if(temp->x == x and temp->y == y)
+        }
+
+        void set(int x, int y, T data)
+        {
+            if(get(x,y) != NULL)
             {
-                return temp->data;
+                auto *temp = hColumns;
+
+                while(temp->x != x and temp->next != NULL)
+                {
+                    temp=temp->next;
+                }
+                while(temp->y != y and temp->down != NULL)
+                {
+                    temp=temp->down;
+                }
+                temp->data = data;
+                return;
+
+            }
+
+            if(x==0 or y==0){
+                cout << "IllegalIndexPosition" << endl;
+                throw;
+            }
+            if(x>columns or y>rows)
+            {
+                cout<< "OutOfRange"<<endl;
+                throw;
             }
             else
             {
-                return 0;
+
+                auto *tempX = hColumns;
+                auto *tempY = hRows;
+                auto *nodo = new Node<T>;
+                int x1 = 0;
+                int y1 = 0;
+                nodo->x = x;
+                nodo->y = y;
+                nodo->data = data;
+
+                for(int xi=0; xi<=columns; xi++)
+                {
+                    if(tempX->x==nodo->x)
+                    {
+                        if(tempX->down==NULL)
+                        {
+                            tempX->down=nodo;
+                        }
+
+                        if(tempX->down->y>nodo->y)
+                        {
+                            nodo->down=tempX->down;
+                            tempX->down=nodo;
+                        }
+                        else if(tempX->down->y<nodo->y)
+                        {
+                            while(tempX->down and tempX->y < nodo->y)
+                            {
+                                tempX = tempX->down;
+                            }
+                            tempX->down=nodo;
+                        }
+                        break;
+                    }
+                    if(tempX->next!=NULL)
+                    {
+                        tempX = tempX->next;
+                    }
+                }
+                for(int yi=0; yi<=rows; yi++)
+                {
+                    if(tempY->y==nodo->y)
+                    {
+                        if(tempY->next==NULL)
+                        {
+                            tempY->next=nodo;
+                        }
+
+                        if(tempY->next->x>nodo->x)
+                        {
+                            nodo->next=tempY->next;
+                            tempY->next=nodo;
+                        }
+                        else if(tempY->next->x<nodo->x)
+                        {
+                            while(tempY->next and tempY->x < nodo->x)
+                            {
+                                tempY = tempY->next;
+                            }
+                            tempY->next=nodo;
+                        }
+
+                        break;
+                    }
+                    if(tempY->down!=NULL)
+                    {
+                        tempY = tempY->down;
+                    }
+
+                }
+                cout<<"Created node in"<<" "<<nodo->x<<" "<<nodo->y<<" with data: "<<nodo->data<<endl;
             }
-        }
-    };
-    Matrix<T> operator*(Matrix<T> other){};
+
+        };
     T get(int x, int y)
     {
         if(x == 0 or y == 0)
@@ -188,34 +185,65 @@ public:
         }
 
     }
-    Matrix<T> operator*(T scalar)
-    {
-        auto *temp = hColumns;
-        for(int i=0; i<=columns; i++)
+        T operator()(int x, int y)
         {
-            if(temp->next)
+            if(x == 0 or y == 0)
             {
-                temp = temp->next;
-                for(int i=0; i<=rows;i++)
-                {
-                    if(temp->down)
-                    {
-                        temp = temp->down;
-                        temp->data *= scalar;
-                    }
-                }
+                cout<<"InvalidIndex"<<endl;
+                throw;
+            }
+            else{
+            auto *temp = hColumns;
+         
+            while(temp->x != x and temp->next != NULL)
+            {
+                temp=temp->next;
+            }
+            while(temp->y != y and temp->down != NULL)
+            {
+                temp=temp->down;
             }
 
-        }
-    };
-    Matrix<T> operator+(Matrix<T> other)
-    {
-        if(columns < other.columns and rows < other.columns)
+             if(temp->x == x and temp->y == y)
+             {
+                return temp->data;
+             }
+             else
+                 {
+                 return 0;
+                 }
+            }
+        };
+        Matrix<T> operator*(Matrix<T> other){};
+        Matrix<T> operator*(T scalar)
         {
-            cout<<"InvalidMatrixB";
-            throw;
-        }
-        else if (columns == other.columns and rows == other.rows){
+            auto *temp = hColumns;
+            for(int i=0; i<=columns; i++)
+            {
+                if(temp->next)
+                {
+                temp = temp->next;
+                    for(int i=0; i<=rows;i++)
+                    {
+                        if(temp->down)
+                        {
+                            temp = temp->down;
+                            temp->data *= scalar;
+                        }
+                    }
+                }
+
+            }
+        };
+        Matrix<T> operator+(Matrix<T> other)
+        {
+            if(columns < other.columns and rows < other.columns)
+            {
+                cout<<"InvalidMatrixB";
+                throw;
+            }
+            else if (columns == other.columns and rows == other.rows){
+            auto resultado = *this;
             auto *temp2 = other.hColumns;
             auto *temp1 = other.hColumns;
             for(int i=0; i<=other.columns; i++)
@@ -229,15 +257,15 @@ public:
 
                         if(temp2->x != 0 and temp2->y != 0)
                         {
-                            if(get(temp2->x, temp2->y) == NULL)
+                            if(resultado(temp2->x, temp2->y) == NULL)
                             {
-                                set(temp2->x, temp2->y, temp2->data);
+                                resultado.set(temp2->x, temp2->y, temp2->data);
                                 temp2 = temp1;
                                 break;
                             }
                             else
                             {
-                                set(temp2->x,temp2->y,(temp2->data+(get(temp2->x, temp2->y))));
+                                resultado.set(temp2->x,temp2->y,(temp2->data+resultado(temp2->x, temp2->y)));
                                 temp2 = temp1;
                                 break;
                             }
@@ -255,13 +283,14 @@ public:
 
                     }
                 }
-            }}
-    };
-    Matrix<T> operator-(Matrix<T> other){};
-    Matrix<T> transposed(){};
-    Matrix<T> operator=(Matrix<T> other){};
+            }
+            return resultado;}
+        };
+        Matrix<T> operator-(Matrix<T> other){};
+        Matrix<T> transposed(){};
+        Matrix<T> operator=(Matrix<T> other){};
 
-    ~Matrix(){};
+        ~Matrix(){};
 };
 
 #endif
